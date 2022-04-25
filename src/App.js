9192dee014205ca1f18acbc3ed2e78131c8e55c6
Joseph Madenjian <joseph.madenjian@gmail.com>
2022-04-25 13:13:24 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from "./components/Nav"
+import React, { Component } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Register from "./views/Register";
+import AlertMessage from "./components/AlertMessage";
+import Login from "./views/Login";
+import Home from "./views/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+  constructor(props){
+      super(props);
+      this.state = {
+          name: null,
+          message: null,
+          category: null,
+          loggedIn: localStorage.getItem('token') ? true : false
+      }
+  }
+
+
+  flashMessage = (message, category) => {
+    this.setState({message,category})
 }
 
-export default App;
+  login = () => {
+    this.setState({loggedIn: true})
+  }
+
+  logout = () => {
+    localStorage.removeItem('token');
+    this.flashMessage('You have Logged Out', 'secondary')
+    this.setState({loggedIn: false})
+  }
+
+  render() {
+    return (
+      <> 
+        <Nav loggedIn={this.state.loggedIn} logUserOut={this.logout}/>
+        <div className="container">
+        {this.state.message ? <AlertMessage category={this.state.category} message={this.state.message} flashMessage={this.flashMessage}/> : null}
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="register" element={<Register flashMessage={this.flashMessage}/>}/>
+            <Route path="login" element={<Login flashMessage={this.flashMessage} login={this.login}/>}/>
+            <Route path="login" element={<Login flashMessage={this.flashMessage} login={this.login}/>}/>
+            
+          </Routes>
+
+        </div>
+      </> // This is the parent tag, a marker for content to be read, not functional
+    ) // this is the return App content closer
+  } // this curly bracket is the App() function closer
+}
