ebb563e05d72b669e70565b3e9acb501e0af9579
Joseph Madenjian <joseph.madenjian@gmail.com>
2022-04-27 16:48:09 -0400

    fixed mobile drop down

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,31 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import { useState } from 'react';
 
 
 export default function Nav(props) {
+
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
             <a className="navbar-brand" href="\">MusicAPI</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="\navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="custom-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
             <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarsExample09">
             <div className="navbar-nav">
-                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                <Link className="nav-link active" aria-current="page" to="/eras">Eras</Link>
-                <Link className="nav-link active" aria-current="page" to="/keys">Key-Signatures</Link>
-                <Link className="nav-link active" aria-current="page" to="/composers">Composers</Link>
-                <Link className="nav-link active" aria-current="page" to="/songs">Songs</Link>
-                <Link className="nav-link active" aria-current="page" to="/create-era">Create-Era</Link>
-                <Link className="nav-link active" aria-current="page" to="/create-key">Create-Key</Link>
-                <Link className="nav-link active" aria-current="page" to="/create-composer">Create-Composer</Link>
-                <Link className="nav-link active" aria-current="page" to="/create-song">Create-Song</Link>
-                { props.loggedIn ? (<><Link className="nav-link active" aria-current="page" to="/" onClick={props.logUserOut}>Logout</Link></>) : (
+                <Link className="nav-link active" onClick={handleNavCollapse} aria-current="page" to="/">Home</Link>
+                <Link className="nav-link active" onClick={handleNavCollapse} aria-current="page" to="/eras">Eras</Link>
+                <Link className="nav-link active" onClick={handleNavCollapse} aria-current="page" to="/keys">Key-Signatures</Link>
+                <Link className="nav-link active" onClick={handleNavCollapse} aria-current="page" to="/composers">Composers</Link>
+                <Link className="nav-link active" onClick={handleNavCollapse} aria-current="page" to="/songs">Songs</Link>
+                <Link className="nav-link active" onClick={handleNavCollapse} aria-current="page" to="/create-era">Create-Era</Link>
+                <Link className="nav-link active" onClick={handleNavCollapse} aria-current="page" to="/create-key">Create-Key</Link>
+                <Link className="nav-link active" onClick={handleNavCollapse} aria-current="page" to="/create-composer">Create-Composer</Link>
+                <Link className="nav-link active" onClick={handleNavCollapse} aria-current="page" to="/create-song">Create-Song</Link>
+                { props.loggedIn ? (<><Link className="nav-link active" onClick={handleNavCollapse} aria-current="page" to="/" onClick={props.logUserOut}>Logout</Link></>) : (
                   <>
-                  <Link className="nav-link active" aria-current="page" to="/register">Register</Link>
-                  <Link className="nav-link active" aria-current="page" to="/login">Login</Link>
+                  <Link className="nav-link active" onClick={handleNavCollapse} aria-current="page" to="/register">Register</Link>
+                  <Link className="nav-link active" onClick={handleNavCollapse} aria-current="page" to="/login">Login</Link>
                   </>
                 )}
             </div>
